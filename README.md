cec923d25e5f96837498b7f26cd2f79950f1743a
Rana Salem <ranatasalem@gmail.com>
2024-06-23 22:25:43 +0100

    apollo server

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Sway API
 A GraphQL API featuring data persistence in Azure Cosmos DB acting as the data access layer for Sway, a social media platform.
+
---
+++ app/graphql/apollo-server.js
@@ -0,0 +1,8 @@
+const { ApolloServer } = require('@apollo/server')
+
+const apolloServer = new ApolloServer({
+  // typeDefs,
+  // resolvers
+})
+
+module.exports = { apolloServer }
