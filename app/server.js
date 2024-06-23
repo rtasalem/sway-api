f4767dd85b688a0b4c8e3e689cdb0a326d46fbfd
Rana Salem <ranatasalem@gmail.com>
2024-06-23 18:27:18 +0100

    skeleton layout

---
+++ app/server.js
@@ -0,0 +1,23 @@
+'use strict'
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+  try {
+    const server = Hapi.server({
+      port: 3100, // replace with env port
+      host: '0.0.0.0'
+    })
+  
+    await server.start()
+    console.log('Server running on %s', server.info.uri)
+    
+  } catch (error) {
+    console.error('Unable to start server:', error)
+    process.exit(1)
+  }
+  process.on('unhandledRejection', (err) => {
+    console.log(err)
+    process.exit(1)
+}
+
+module.exports = { server }
