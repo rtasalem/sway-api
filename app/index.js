666dd58c96e594333103d48d096b887f9fd9dc81
Rana Salem <ranatasalem@gmail.com>
2024-06-23 22:09:41 +0100

    set up server & scripts

--- app/index.js
+++ app/index.js
@@ -1,22 +1,26 @@
 const { server } = require('./server')
 
 const init = async () => {
-  await server.register({
-    options: {
-      path: '/graphql',
-      context: ({ request }) => {
-        return { headers: request.headers }
-      }
+  try {
+    // await server.register({
+    //   options: {
+    //     path: '/graphql',
+    //     context: ({ request }) => {
+    //       return { headers: request.headers }
+    //     }
+    //   }
+    // })
+
+    await server.start()
+    console.log('Server running on %s', server.info.uri)
+  } catch (error) {
+    process.on('unhandledRejection', err => {
+      console.log(err)
+      process.exit(1)
     }
-  })
 
-  await server.start()
-  console.log('Server running on %s', server.info.uri)
+    )
+  }
 }
-
-process.on('unhandledRejection', err => {
-  console.log(err)
-  process.exit(1)
-})
 
 init()
--- app/server.js
+++ app/server.js
@@ -1,23 +1,16 @@
 'use strict'
 const Hapi = require('@hapi/hapi')
 
-const init = async () => {
-  try {
-    const server = Hapi.server({
-      port: 3100, // replace with env port
-      host: '0.0.0.0'
-    })
+const server = Hapi.server({
+  port: 3100,
+  host: '0.0.0.0'
+})
 
-    await server.start()
-    console.log('Server running on %s', server.info.uri)
+const routes = [].concat(
+  require('./routes/healthy'),
+  require('./routes/healthz')
+)
 
-  } catch (error) {
-    console.error('Unable to start server:', error)
-    process.exit(1)
-  }
-  process.on('unhandledRejection', (err) => {
-    console.log(err)
-    process.exit(1)
-  })
+server.route(routes)
 
-  module.exports = { server }
+module.exports = { server }
